3d8788e7a8c3a213cd27f0b204367ebbf1fcabf7
mstars_lab2_06 <mstars_lab2_06@mstars-lab2-06s-iMac.local>
2022-05-10 17:58:17 +0800

    update

--- api/books.js
+++ api/books.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const { body, validationResult } = require("express-validator");
+
+app.use(express.json());
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("view options", { layout: false });
 
 app.get("/", function (req, res) {
   fs.readFile("../data/book.json", (error, data) => {
     if (error) {
       throw error;
     } else {
-      const publisher = JSON.parse(data);
-      res.send(publisher);
+      const data1 = JSON.parse(data);
+      // res.send(data1);
+      console.log(data1);
+      res.render("addedbook", { books: data1 });
     }
   });
 });
