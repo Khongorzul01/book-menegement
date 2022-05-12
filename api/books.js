8ab760e1dab1b16b6e6204c712b9fb75689cf1bd
Khongorzul <hhongoroo0230@gmail.com>
2022-05-12 11:44:53 +0800

    update

--- api/books.js
+++ api/books.js
@@ -9,13 +9,38 @@ app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("view options", { layout: false });
 
-app.get("/", function (req, res) {
+app.get("/books", function (req, res) {
   fs.readFile("../data/book.json", (error, data) => {
     if (error) {
       throw error;
     } else {
       const data1 = JSON.parse(data);
+      
+      res.render("book", { books: data1 });
+    }
+  });
+});
+
+app.get("/add", function (req, res) {
+  fs.readFile("../data/book.json", (error, data) => {
+    if (error) {
+      throw error;
+    } else {
+      const data1 = JSON.parse(data);
+      
       res.render("addedbook", { books: data1 });
+    }
+  });
+});
+
+app.get("/delete/isbn_id", function (req, res) {
+  fs.readFile("../data/book.json", (error, data) => {
+    if (error) {
+      throw error;
+    } else {
+      const data1 = JSON.parse(data);
+      
+      res.render("bookdelete", { books: data1 });
     }
   });
 });
